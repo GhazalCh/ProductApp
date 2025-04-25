ff4ee6d69b18110c228044eccad9f0efdd296132
GhazalCh <106462845+GhazalCh@users.noreply.github.com>
2025-04-25 12:14:55 +0330

    Update Program.cs

--- AspNetCoreProductApi/Program.cs
+++ AspNetCoreProductApi/Program.cs
@@ -1,16 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hostinh;
+using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using AspNetCoreProductApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //add services
-builder.Services.AddContext<AppContext>(options =>
+builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplore();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
